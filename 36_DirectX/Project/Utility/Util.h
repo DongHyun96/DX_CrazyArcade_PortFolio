4860229a0f0834854563f8de9239d545b2d67693
DongHyun96 <wadanka09@gmail.com>
2023-09-30 22:40:44 +0900

    Update Util.h
    유틸에 반복되는 코드 함수로 추가

--- 36_DirectX/Project/Utility/Util.h
+++ 36_DirectX/Project/Utility/Util.h
@@ -13,13 +13,29 @@ namespace Util
 		return aT->zDepth > bT->zDepth;
 	}
 
-	static Vector2 ConvertBoardIdxToWorldPos(UINT x, UINT y)
+	static Vector2 ConvertBoardIdxToWorldPos(const UINT& x, const UINT& y)
 	{
 		return Vector2
 		(
 			WIN_WIDTH / (MAP_COL * 2.f)  + x * (WIN_WIDTH / MAP_COL)  - WIN_CENTER.x,
 			WIN_HEIGHT / (MAP_ROW * 2.f) + y * (WIN_HEIGHT / MAP_ROW) - WIN_CENTER.y
 		);
+	}
+
+	static Vector2 ConvertBoardIdxToWorldPos(const pair<UINT, UINT>& xy)
+	{
+		return ConvertBoardIdxToWorldPos(xy.first, xy.second);
+	}
+
+	static void SetTransformToGameBoard(Transform* target, const UINT& x, const UINT& y)
+	{
+		target->translation = ConvertBoardIdxToWorldPos(x, y);
+		target->SetParent(GM->GetGameFieldTransform());
+	}
+
+	static void SetTransformToGameBoard(Transform* target, const pair<UINT, UINT> boardXY)
+	{
+		SetTransformToGameBoard(target, boardXY.first, boardXY.second);
 	}
 }
 
