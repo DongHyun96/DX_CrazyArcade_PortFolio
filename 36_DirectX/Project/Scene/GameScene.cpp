ec4df95999399c4d8597f6582fca65f5fbf11cd6
DongHyun96 <wadanka09@gmail.com>
2023-10-02 00:39:01 +0900

    etc.

--- 36_DirectX/Project/Scene/GameScene.cpp
+++ 36_DirectX/Project/Scene/GameScene.cpp
@@ -5,16 +5,14 @@ GameScene::GameScene()
 {
 	mainUI = new Object(WIN_SIZE, L"InGame/UI/mapToolMap.png");
 	mainUI->translation = WIN_CENTER;
+	mainUI->zDepth = 1300.f;
 
 	mapBoundary = new ColliderRect(WIN_SIZE);
 	mapBoundary->translation = WIN_CENTER;
 	mapBoundary->SetLabel("MapBoundary");
 
 	tileManager = new TileManager;
-
-	//block = new Block({ 5, 6 }, L"InGame/Village/Objects/box.png", { 3, 1 }, { 2, 1 });
-	block = new Block({ 5, 6 }, L"InGame/Village/Objects/Stone.png", {15 , 1 }, { 4, 1 }, Vector2(WIN_WIDTH / MAP_COL, WIN_HEIGHT / MAP_ROW * 2.f));
-	//block = new Block({ 5, 6 }, L"InGame/Village/Objects/bush.png");
+	blockManager = new BlockManager;
 
 }
 
@@ -23,7 +21,7 @@ GameScene::~GameScene()
 	delete mainUI;
 	delete mapBoundary;
 	delete tileManager;
-	delete block;
+	delete blockManager;
 }
 
 void GameScene::Update()
@@ -39,8 +37,8 @@ void GameScene::Update()
 	}
 
 	tileManager->Update();
-
-	block->Update();
+	blockManager->Update();
+	
 }
 
 void GameScene::Render()
@@ -54,7 +52,7 @@ void GameScene::Render()
 		return;
 	}
 
-	//tileManager->Render();
-	block->Render();
+	tileManager->Render();
+	blockManager->Render();
 
 }
