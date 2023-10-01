916affe31b5240f9c41310d0f60b8acb928c495c
DongHyun96 <wadanka09@gmail.com>
2023-10-02 00:35:01 +0900

    Update Util.h
    Coord 구조체 추가 / RendererZOrder 비교 삭제 / 기타 함수 파라미터 수정

--- 36_DirectX/Project/Utility/Util.h
+++ 36_DirectX/Project/Utility/Util.h
@@ -3,15 +3,11 @@
 
 namespace Util
 {
-	static bool CompareRendererZorder(Renderer* a, Renderer* b)
+	struct Coord
 	{
-		Transform* aT = dynamic_cast<Transform*>(a);
-		Transform* bT = dynamic_cast<Transform*>(b);
-
-		assert(aT && bT);
-
-		return aT->zDepth > bT->zDepth;
-	}
+		UINT x;
+		UINT y;
+	};
 
 	static Vector2 ConvertBoardIdxToWorldPos(const UINT& x, const UINT& y)
 	{
@@ -22,9 +18,9 @@ namespace Util
 		);
 	}
 
-	static Vector2 ConvertBoardIdxToWorldPos(const pair<UINT, UINT>& xy)
+	static Vector2 ConvertBoardIdxToWorldPos(const Coord& xy)
 	{
-		return ConvertBoardIdxToWorldPos(xy.first, xy.second);
+		return ConvertBoardIdxToWorldPos(xy.x, xy.y);
 	}
 
 	static void SetTransformToGameBoard(Transform* target, const UINT& x, const UINT& y)
@@ -33,9 +29,53 @@ namespace Util
 		target->SetParent(GM->GetGameFieldTransform());
 	}
 
-	static void SetTransformToGameBoard(Transform* target, const pair<UINT, UINT> boardXY)
+	static void SetTransformToGameBoard(Transform* target, const Coord& boardXY)
 	{
-		SetTransformToGameBoard(target, boardXY.first, boardXY.second);
+		SetTransformToGameBoard(target, boardXY.x, boardXY.y);
+	}
+
+	template <typename T>
+	static T GetRandom(const T& start, const T& end)
+	{
+		if (start >= end)
+			return (T)0;
+
+		int range = (int)(end - start + 1);
+
+		return (T)(start + rand() % range);
+	}
+
+	template <typename T>
+	static void Clamp(T& target, const T& floor, const T& ceil)
+	{
+		if (target < floor) target = floor;
+		else if (target > ceil) target = ceil;
+	}
+
+	template <typename T>
+	static T MappingtoNewRange(T num, T srcMin, T srcMax, T dstMin, T dstMax)
+	{
+		if (srcMin > srcMax)
+			return T();
+
+		if (dstMin > dstMax)
+			return T();
+
+		if (srcMin == srcMax)
+			return (T)(((num - srcMin) * (dstMax - dstMin)) + dstMin);
+
+		return (T)(((num - srcMin) * (dstMax - dstMin)) / (srcMax - srcMin) + dstMin);
+	}
+
+	static float Lerp(float src, float dst, float alpha)
+	{
+		if (alpha >= 1)
+			return dst;
+
+		if (alpha <= 0)
+			return src;
+
+		return (1 - alpha) * src + alpha * dst;
 	}
 }
 
