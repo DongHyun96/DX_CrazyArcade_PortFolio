e438721b2cd23c6313b2fdc5554f6563c839aa02
DongHyun96 <wadanka09@gmail.com>
2023-10-02 00:35:43 +0900

    Block 인터렉션 기능 추가

--- 36_DirectX/Project/GameMap/Block.cpp
+++ 36_DirectX/Project/GameMap/Block.cpp
@@ -1,15 +1,17 @@
 #include "Framework.h"
 #include "Block.h"
 
-Block::Block(pair<UINT, UINT> boardXY, wstring texFile, pair<UINT, UINT> frameXY, pair<UINT, UINT> targetXY, Vector2 texWorldSize, BlockProperty bProp)
+Block::Block(Util::Coord boardXY, wstring texFile, Util::Coord frameXY, Util::Coord targetXY, Vector2 texWorldSize, BlockProperty bProp)
 	:breakable(bProp.breakable), movable(bProp.movable), hidable(bProp.hidable)
 {
+	boardPos = { boardXY.x, boardXY.y };
+	
 	texWorldSize.y += Y_OFFSET; // offset 조정
 
-	rectBody	= new ColliderRect(Vector2(WIN_WIDTH / MAP_COL, WIN_HEIGHT / MAP_ROW));
-	texObj		= new Object(texWorldSize, texFile, frameXY.first, frameXY.second, targetXY.first, targetXY.second);
+	rectBody	= new ColliderRect(CELL_WORLD_SIZE);
+	texObj		= new Object(texWorldSize, texFile, frameXY.x, frameXY.y, targetXY.x, targetXY.y);
 
-	texObj->translation.y += Y_OFFSET / 2; // offset 조정
+	texObj->translation.y += (texWorldSize.y - CELL_WORLD_SIZE.y) * 0.5f; // local translation으로 tex의 위치 조정
 
 	texObj->SetParent(rectBody); // 실제 body를 texture가 따라가게끔 처리할 것임
 
@@ -29,8 +31,12 @@ void Block::Update()
 	if (!isActive)
 		return;
 
+	Move();
+	HandleBushInteract();
+
 	rectBody->Update();
 	texObj->Update();
+
 }
 
 void Block::Render()
@@ -40,16 +46,116 @@ void Block::Render()
 
 	rectBody->Render();
 	texObj->Render();
+
+	//Debug();
 }
 
 void Block::PlayBushInteraction()
 {
+	if (currentlyBushing)
+		return;
+
+	currentlyBushing = true;
 }
 
-void Block::Move(Vector2 destination)
+void Block::Move(const UINT& destBoardCoordX, const UINT& destBoardCoordY)
 {
+	if (Move(Util::ConvertBoardIdxToWorldPos({ destBoardCoordX, destBoardCoordY })))
+		boardPos = { destBoardCoordX, destBoardCoordY };
+}
+
+void Block::Move(const Util::Coord& destCoord)
+{
+	if (Move(Util::ConvertBoardIdxToWorldPos({ destCoord.x, destCoord.y })))
+		boardPos = destCoord;
+}
+
+bool Block::Move(Vector2 destination)
+{
+	if (!movable)
+		return false;
+
+	if (currentlyMoving)
+		return false;
+
+	currentlyMoving = true;
+	this->destination = destination;
+
+	return true;
 }
 
 void Block::ApplyDamage()
 {
+	if (!isActive)
+		return;
+
+	hp--;
+
+	if (hp <= 0)
+		isActive = false;
+}
+
+void Block::Move()
+{
+	if (!currentlyMoving || !movable)
+		return;
+
+	static const float speed = 400.f;
+	
+	Vector2 direction = destination - rectBody->translation;
+
+	float leftDist = direction.Length();
+
+	if (leftDist < 1.f) // 도착
+	{
+		rectBody->translation = destination;
+		currentlyMoving = false;
+		return;
+	}
+
+	rectBody->translation += direction.GetNormal() * speed * Time::Delta();
+}
+
+void Block::HandleBushInteract()
+{
+	if (!hidable || !currentlyBushing)
+		return;
+
+	interactTime += Time::Delta();
+
+	// 1초를 0.25로 쪼개어 interaction을 줄 예정, texObj만 local translation x값을 조정하여 양옆으로 왔다갔다만 할 예정
+	if		(interactTime < 0.15f) texObj->translation.x = -10.f;
+	else if (interactTime < 0.3f)	texObj->translation.x = 10.f;
+	else if (interactTime < 0.45f) texObj->translation.x = -10.f;
+	else if (interactTime < 0.6f)	texObj->translation.x = 10.f;
+	else
+	{
+		interactTime -= 0.6f;
+		texObj->translation.x = 0.f;
+		currentlyBushing = false;
+	}
+
+}
+
+void Block::Debug()
+{
+	assert(label != "");
+
+	if (ImGui::BeginMenu(label.c_str()))
+	{
+		ImGui::InputInt2("BoardPos", (int*)&boardPos);
+
+		ImGui::InputFloat2("Translation", (float*)&rectBody->translation);
+
+		Vector2 gp = rectBody->GlobalPosition();
+		ImGui::InputFloat2("worldPos", (float*)&gp);
+
+		ImGui::InputFloat2("Destination", (float*)&destination);
+
+		
+
+		ImGui::EndMenu();
+	}
+
+
 }
--- 36_DirectX/Project/GameMap/Block.h
+++ 36_DirectX/Project/GameMap/Block.h
@@ -15,9 +15,9 @@ public:
 	// texWorldSize에 디폴트로 y offset을 조금 두어 머리부분이 뒤로 조금 넘어가게끔 미세하게 조정할 것임
 	Block
 	(
-		pair<UINT, UINT> boardXY, wstring texFile, 
-		pair<UINT, UINT> frameXY = {1, 1},
-		pair<UINT, UINT> targetXY = {1, 1},
+		Util::Coord boardXY, wstring texFile, 
+		Util::Coord frameXY  = {1, 1},
+		Util::Coord targetXY = {1, 1},
 		Vector2 texWorldSize = Vector2(WIN_WIDTH / MAP_COL, WIN_HEIGHT / MAP_ROW),
 		BlockProperty bProp = {}
 	);
@@ -29,16 +29,39 @@ public:
 
 	void PlayBushInteraction();
 
-	void Move(Vector2 destination);
+	void Move(const UINT& destBoardCoordX, const UINT& destBoardCoordY);
+	void Move(const Util::Coord& destCoord);
 
 	void ApplyDamage();
 
+	void Debug();
+	void SetLabel(const string& label) { this->label = label; }
+
+public:
+
+	Collider* GetBody() { return rectBody; }
+	Vector2 GetTranslation() const { return rectBody->translation; }
+	Util::Coord GetBoardPos() const { return boardPos; }
 	bool& IsActive() { return isActive; }
 
 private:
 
-	Collider* rectBody{};
-	Object* texObj{};
+	bool Move(Vector2 destination);
+	void Move();
+
+	void HandleBushInteract();
+
+
+private:
+
+private:
+	
+	string label{};
+
+	Util::Coord	boardPos{};
+
+	Collider*	rectBody{};
+	Object*		texObj{};
 
 	bool isActive = true;
 
@@ -48,5 +71,13 @@ private:
 
 	UINT hp{};
 	Animation* destroyedAnim{}; // BlockManager에서 받아옴
+
+	// Movable 관련
+	Vector2 destination{};
+	bool currentlyMoving{};
+
+	// Bush Interaction 관련
+	bool currentlyBushing{};
+	float interactTime{};
 
 };
