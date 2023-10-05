8645031b9bffa2c965707d7e6a29fb4726afe230
DongHyun96 <wadanka09@gmail.com>
2023-10-05 22:50:26 +0900

    맵 바운더리값 저장

--- 36_DirectX/Project/Manager/GameManager.cpp
+++ 36_DirectX/Project/Manager/GameManager.cpp
@@ -9,6 +9,13 @@ GameManager::GameManager()
 	gameFieldTransform->scale = Vector2(0.76f, 0.88f);
 	gameFieldTransform->translation = Vector2(768.f, 538.f);
 
+	Vector2 LT = Util::ConvertBoardIdxToWorldPos(0, MAP_ROW - 1);
+	Vector2 RB = Util::ConvertBoardIdxToWorldPos(MAP_COL - 1, 0);
+	
+	mapL = LT.x - CELL_WORLD_SIZE.x * 0.5f;
+	mapT = LT.y + CELL_WORLD_SIZE.y * 0.5f;
+	mapR = RB.x + CELL_WORLD_SIZE.x * 0.5f;
+	mapB = RB.y - CELL_WORLD_SIZE.y * 0.5f;
 }
 
 GameManager::~GameManager()
--- 36_DirectX/Project/Manager/GameManager.h
+++ 36_DirectX/Project/Manager/GameManager.h
@@ -1,6 +1,7 @@
 #pragma once
 
 class Transform;
+class Character;
 
 class GameManager
 {
@@ -18,15 +19,31 @@ public:
 	}
 
 	void Update();
+	
+	void SetPlayer(Character* player) { this->player = player; }
+	Character* GetPlayer() const { return player; }
 
 public:
 
 	Transform* GetGameFieldTransform() const { return gameFieldTransform; }
 	bool IsEditMode() const { return editMode; }
 
+	float GetMapL() const { return mapL; }
+	float GetMapR() const { return mapR;}
+	float GetMapT() const { return mapT; }
+	float GetMapB() const { return mapB; }
+
 private:
+
+	Character* player{};
 
 	Transform* gameFieldTransform{};
 	
 	bool editMode = false;
+
+	float mapL{};
+	float mapR{};
+	float mapT{};
+	float mapB{};
+
 };
